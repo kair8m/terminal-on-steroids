df2bc5ab4225476618242badeca48f46c55f4d57
Kair <kair8m@gmail.com>
2022-11-04 17:03:58 +0100

    add working python installation script

--- scripts/install_python.sh
+++ scripts/install_python.sh
@@ -1,12 +1,30 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
+PYTHON_VERSION="3.10"
 
-if [ "$(uname)" == "Darwin" ]; then
-    # TODO: install python for Mac OS X platform
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    # TODO: install python for GNU/Linux platform
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-    # TODO: install python for 32 bits Windows NT platform
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-    # TODO: install python for 64 bits Windows NT platform
-fi
+function linux_install() {
+    if (($EUID != 0)); then
+        if [[ -t 1 ]]; then
+            sudo "$0" "$@"
+        else
+            exec 1>output_file
+            gksu "$0 $@"
+        fi
+        exit
+    fi
+    apt-get install software-properties-common -yy
+    add-apt-repository ppa:deadsnakes/ppa -yy
+    apt-get update -yy
+    apt-get install python${PYTHON_VERSION} -yy
+}
+
+case "$OSTYPE" in
+  linux*)   linux_install ;;
+  darwin*)  echo "Mac OS" ;; 
+  win*)     echo "Windows" ;;
+  msys*)    echo "MSYS / MinGW / Git Bash" ;;
+  cygwin*)  echo "Cygwin" ;;
+  bsd*)     echo "BSD" ;;
+  solaris*) echo "Solaris" ;;
+  *)        echo "unknown: $OSTYPE" ;;
+esac
