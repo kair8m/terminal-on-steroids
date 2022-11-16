06221623768cb803003149f6ef3f940e11d52c19
Kair <kair8m@gmail.com>
2022-11-16 11:52:08 +0100

    setup instruction for linux is almost complete

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,16 +3,17 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 function setup_zsh() {
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-    git clone https://github.com/zsh-users/zsh-autosuggestions          "${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git  "${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
-    git clone https://github.com/joshskidmore/zsh-fzf-history-search    "${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}"/plugins/zsh-fzf-history-search
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git    "${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}"/themes/powerlevel10k
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended > /dev/null
+    git clone https://github.com/zsh-users/zsh-autosuggestions          "${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions > /dev/null
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git  "${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting > /dev/null
+    git clone https://github.com/joshskidmore/zsh-fzf-history-search    "${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}"/plugins/zsh-fzf-history-search > /dev/null
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git    "${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}"/themes/powerlevel10k > /dev/null
     DOTFILES_DIR=$(realpath "$SCRIPT_DIR/../dotfiles")
     cp -r "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc"
     cp -r "$DOTFILES_DIR/.pythonrc" "$HOME/.pythonrc"
     cp -r "$DOTFILES_DIR/.gitconfig" "$HOME/.gitconfig"
     cp -r "$DOTFILES_DIR/.fdignore" "$HOME/.fdignore"
+    cp -r "$DOTFILES_DIR/.profile" "$HOME/.profile"
     mkdir -p "$HOME/.config/lazygit"
     cp -r "$DOTFILES_DIR/lazygit/config.yml" "$HOME/.config/lazygit/"
     mkdir -p "$HOME/.config/dotfiles"
@@ -25,32 +26,39 @@ function setup_zsh() {
 }
 
 function setup_neovim() {
+    echo "Setting up neovim..."
+    source "$HOME/.profile"
     mkdir -p "$HOME/.config"
     NVIM_DIR=$(realpath "$SCRIPT_DIR/../nvim")
     cp -r "$NVIM_DIR" "$HOME/.config"
-    nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
-    nvim --headless -c 'autocmd User PackerComplete quitall' -c 'TSUpdate'
-    nvim --headless -c 'autocmd User PackerComplete quitall' -c 'TSUpdateSync'
+    # nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+    # nvim --headless -c 'autocmd User PackerComplete quitall' -c 'TSUpdate'
+    # nvim --headless -c 'autocmd User PackerComplete quitall' -c 'TSUpdateSync'
 
 }
 
 function setup_on_linux() {
-    sudo apt-get update -yy
-    sudo apt-get install curl wget fzf ripgrep tmux make cmake g++ gcc bat build-essential zip unzip dpkg tmux fd-find -yy
+    echo "performing apt-get update && apt-get upgrade..."
+    sudo apt-get update -yy > /dev/null
+    sudo apt-get install curl wget fzf ripgrep tmux make cmake g++ gcc bat build-essential zip unzip dpkg tmux fd-find -yy > /dev/null
 
     cd /tmp/ || exit
+
+    echo "Installing lazygit..."
 
     LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
     curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
     sudo tar xf lazygit.tar.gz -C /usr/local/bin lazygit
     rm -rf lazygit.tar.gz
 
+    echo "Installing git delta..."
+
     wget https://github.com/dandavison/delta/releases/download/0.14.0/git-delta_0.14.0_amd64.deb
     sudo dpkg -i git-delta_0.14.0_amd64.deb
     rm git-delta_0.14.0_amd64.deb -rf
     cd - || exit
 
-    sudo apt-get install zsh -yy
+    sudo apt-get install zsh -yy > /dev/null
 
     mkdir -p "${HOME}"/.local/bin
     
